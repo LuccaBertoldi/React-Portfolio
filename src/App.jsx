ab99de5f4cd22e66dc18add130589158575892c3
LuccaBertoldi <lbertoldi93@gmail.com>
2024-10-04 09:00:31 -0400

    error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
+import Header from './components/header';
+import Footer from './components/footer';
+import About from './components/about';
+import Portfolio from './components/portfolio';
+import Contact from './components/contact';
+import Resume from './components/resume';
 
 
 const App = () => {
